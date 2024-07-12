0d63e4971fc2b1f75574ba952e58cf1ae6f983af
Finw7 <finwall26@gmail.com>
2024-07-12 03:13:36 -0400

    Commit

---
+++ Project0banking/src/main/java/models/Transaction.java
@@ -0,0 +1,56 @@
+package models;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+public class Transaction {
+    private int id;
+    private int accountId;
+    private BigDecimal amount;
+    private String type;
+    private Timestamp timestamp;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int account_id) {
+        this.accountId = account_id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+
+}
---
+++ Project0banking/src/main/java/models/User.java
@@ -0,0 +1,65 @@
+package models;
+
+public class User {
+    private Integer userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+
+
+    public User(){
+
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
