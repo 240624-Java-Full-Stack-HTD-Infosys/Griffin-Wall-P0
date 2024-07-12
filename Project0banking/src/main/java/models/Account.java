dcc9b68f80aab486dd0bff1e5d1e9f102086e9dd
Finw7 <finwall26@gmail.com>
2024-07-12 03:13:05 -0400

    Commit

---
+++ Project0banking/src/main/java/models/Account.java
@@ -0,0 +1,42 @@
+package models;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private int id;
+    private int userId;
+    private String accountName;
+    private BigDecimal balance;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
